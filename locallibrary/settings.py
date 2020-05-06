0d7a7a23ae983543370369e7be8ac91e63355e8d
zhang xiaoyou <discovery131794@163.com>
2020-05-06 16:54:36 +0800

    modify allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')-@@l61ggc+bmqv8*au#idw6^n)8%ic5e!z)4e0cm!a@!rd)km'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
